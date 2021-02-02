3656a288ca8063426667ec7ef1bb1900b38ec8de
niloycj7 <niloyrahman31@gmail.com>
2021-02-02 19:35:31 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy:
+// 0
+//""
+//undefined
+//null
+//NaN
+//false
+//Truthy:
+//'0' , ' ' , [], {}
+// const name = "Solaiman";
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
